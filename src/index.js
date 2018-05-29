b4f27866e2c55e8ea9e83b0a8b2df7dc8e2f7208
TS000 <tylersnell@me.com>
2018-05-28 22:53:59 -0600

    updated hero font

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import styled, { injectGlobal } from 'styled-components'
 import App from './layouts/App'
 import registerServiceWorker from './registerServiceWorker'
+import WebFont from 'webfontloader'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// WebFont is co-developed by Google and Typekit. It won't add blocking CSS to your app.
+WebFont.load({
+    google: {
+        families: ['Lato']
+    }
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
