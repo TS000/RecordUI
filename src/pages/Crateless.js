1b968d1f36d348351be82296fa58adf7666b3a33
TS000 <tylersnell@me.com>
2018-07-04 13:19:17 -0600

    added account and markets components to crateless layout

--- src/pages/Crateless.js
+++ src/pages/Crateless.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Crate from '../components/Crate'
+import Account from '../components/Account'
+import Styled from 'styled-components'
+import Markets from '../components/Markets'
+
+const AppWrap = Styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+`
 
 const Crateless = () => {
-  return <Crate />
+  return (
+    <Fragment>
+      <AppWrap>
+        <Account />
+        <Crate />
+        <Markets />
+      </AppWrap>
+    </Fragment>
+  )
 }
 export default Crateless
